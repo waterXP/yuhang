bd73745a6a811a242bc1ecaa676c70a62233a70f
陈文杰 <wutianba@sina.com>
2017-07-25 15:14:26 +0800

    Fix bug

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -4,46 +4,10 @@ import './Account.scss'
 
 class Account extends Component {
   static propTypes = {
-    // content: PropTypes.string.isRequired,
-    // type: PropTypes.number.isRequired,
     chooseBankName: PropTypes.string
   }
 
   render () {
-    // let { content, type, chooseBankName } = this.props
-    // let length = content.length
-    // let marked = ''
-    // let hiddenControl = type === 2 ? 4 : 8
-    // let first = type === 2 ? 3 : 4
-    // let last = ''
-
-    // let isMail = content.indexOf('@') > 0
-    // if (isMail) {
-    //   [first, last] = content.split('@')
-    //   hiddenControl = first.length > 4 ? 4 : 1
-    //   first = first.substr(0, hiddenControl) +
-    //   new Array(first.length - hiddenControl + 1).join('*')
-    //   marked = first + '@'
-    //   marked += last || ''
-    // } else {
-    //   if (length > 8) {
-    //     marked = content.substr(0, first)
-    //     let rest = length - first
-    //     let count = ~~(rest / 4)
-
-    //     last = rest % 4
-    //     if (last === 0) {
-    //       last = 4
-    //       count--
-    //     }
-    //     marked += new Array(count + 1).join(' ****')
-    //     marked += ' ' + content.substr(length - last)
-    //   } else {
-    //     marked = content.substr(0, 1) +
-    //     new Array(content.length).join('*')
-    //   }
-    // }
-
     return (
       <div className='wm-account'>
         <label>账号</label><h6>{this.props.chooseBankName}</h6>
