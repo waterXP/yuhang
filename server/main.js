ea84797a645483f4468a982da7cdc899a7d9bc69
吴俊锋 <wujunfeng@wujunfengdeMacBook-Pro.local>
2017-08-04 14:47:32 +0800

    Server

--- server/main.js
+++ server/main.js
@@ -40,7 +40,7 @@ if (project.env === 'development') {
 
   app.use('/api', proxy({
     // target: 'http://120.77.209.222/wagestest/',
-    target: 'http://172.16.2.146:8080/',
+    target: 'http://192.168.0.112:8080/',
     pathRewrite: { '^/api' : 'wages' },
     changeOrigin: true
   }))
